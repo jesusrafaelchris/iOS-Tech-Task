dfe8c18ab4958721addba1a96e0d0a02dc98e45d
Christian Grinling <grinlingc@gmail.com>
2024-04-01 21:37:22 +0100

    Added theme colours as static variables

---
+++ MoneyBox/Extensions/UIColor+Ext.swift
@@ -0,0 +1,7 @@
+import UIKit
+
+extension UIColor {
+    static let accent = UIColor(named: "AccentColor")
+    static let border = UIColor(red: 0.70, green: 0.81, blue: 0.84, alpha: 1.00)
+    static let secondary = UIColor(red: 0.40, green: 0.63, blue: 0.67, alpha: 1.00)
+}

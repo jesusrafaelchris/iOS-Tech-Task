766f5c8c05b1da27a819709ec412bfea351f3193
Christian Grinling <grinlingc@gmail.com>
2024-04-01 21:54:55 +0100

    Removed isUserLoggedIn check

--- MoneyBox/SceneDelegate.swift
+++ MoneyBox/SceneDelegate.swift
@@ -10,15 +10,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
         
-        if sessionManager.isUserLoggedIn {
-            window.rootViewController = UINavigationController(
-                rootViewController: AccountsViewController(viewModel: AccountsViewModel(dataProvider: DataProvider(), sessionManager: SessionManager()))
+        window.rootViewController = UINavigationController(
+            rootViewController: LoginViewController(viewModel: LoginViewModel(dataProvider: DataProvider(), sessionManager: SessionManager()))
             )
-        } else {
-            window.rootViewController =  UINavigationController(
-                rootViewController: LoginViewController(viewModel: LoginViewModel(dataProvider: DataProvider(), sessionManager: SessionManager()))
-                )
-        }
 
         self.window = window
         window.makeKeyAndVisible()

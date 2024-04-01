274a9aa8697dfcdbe59f64c8f72311ff5dac5177
Christian Grinling <grinlingc@gmail.com>
2024-04-01 21:39:35 +0100

    Added check if token nil to show login screen

--- MoneyBox/SceneDelegate.swift
+++ MoneyBox/SceneDelegate.swift
@@ -1,14 +1,26 @@
 import UIKit
+import Networking
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
+    
     var window: UIWindow?
-
+    let sessionManager = SessionManager()
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-         guard let windowScene = (scene as? UIWindowScene) else { return }
-         window = UIWindow(frame: windowScene.coordinateSpace.bounds)
-         window?.windowScene = windowScene
-         window?.rootViewController = LoginViewController()
-         window?.makeKeyAndVisible()
-     }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        let window = UIWindow(windowScene: windowScene)
+        
+        if sessionManager.isUserLoggedIn {
+            window.rootViewController = UINavigationController(
+                rootViewController: AccountsViewController(viewModel: AccountsViewModel(dataProvider: DataProvider(), sessionManager: SessionManager()))
+            )
+        } else {
+            window.rootViewController =  UINavigationController(
+                rootViewController: LoginViewController(viewModel: LoginViewModel(dataProvider: DataProvider(), sessionManager: SessionManager()))
+                )
+        }
+
+        self.window = window
+        window.makeKeyAndVisible()
+    }
 }
